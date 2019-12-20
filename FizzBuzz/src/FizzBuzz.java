837df26acaea1369d3a81310c006ae56ec26271d
marsh <m.cowles21191@gmail.com>
2019-12-20 02:02:08 -0500

    added comments and a final sysout line

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -11,10 +11,15 @@ public static void main (String [] args) {
 	List<String> fizzBuzz = new ArrayList<>();
 	
 	for(int i = 1; i <= 100; i++) {
+		
+		//check if array item is NOT divisible by 3 or 5 first, then add a number
+		//possible we could use "15" here since that is a common denominator
+		
 		if(i%3 != 0 && i%5 != 0) {
 		fizzBuzz.add(Integer.toString(i));
 		}
 		
+		//if it is divisible by 3 or 5, we begin our "fizzbuzz" additions
 		if(i%3 == 0 && i%5 ==0) {
 		    fizzBuzz.add("FizzBuzz");
 		}else if(i%5 == 0) {
@@ -25,6 +30,9 @@ public static void main (String [] args) {
 	    
 	    System.out.println(fizzBuzz.get(i-1));
 	}
+	System.out.println("FizzBuzz complete.");
+	
+	//my first Git test comment
 	
   }
 }
